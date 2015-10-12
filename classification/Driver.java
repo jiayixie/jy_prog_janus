4330af16d68b3e02d963536493568a632cb355ca
jiayixie <jiayi.seis@gmail.com>
2015-10-12 09:38:54 -0600

    Oct12_backup

---
+++ classification/Driver.java
@@ -0,0 +1,12 @@
+
+public class Driver{
+    public static void main(String[] args)
+    {
+        //read in data
+        //do Kmean clustering
+        //output clustered data
+
+    }// main
+
+}// Driver
+
---
+++ classification/KmeanData.java
@@ -0,0 +1,17 @@
+// this is used to cluster data. it is unsupervised learning
+//
+public class KmeanData {
+
+// instance variable
+//
+
+// constructor
+// --set number of centroid, and set number of iteration
+
+// method
+// computeCentroid
+// initialCentroid
+// findClosestCentroid
+// kmeanClustering
+
+}//KmeanClustering
